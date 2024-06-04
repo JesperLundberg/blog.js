195b4dd375a9ce34e9569ed9b36248ad75486fe7
Jesper Lundberg <lundberg.jesper@gmail.com>
2024-06-04 13:37:51 +0200

    Add astro frontend (#4)
    * Add astro frontend
    
    * Ignore xml.js files from linting
    
    * Start the refactor of the template
    
    * Move the mastodon and github links to constants
    
    * Fix styling (or ignore the file)
    
    * Remove twitter

---
+++ src/frontend/src/consts.ts
@@ -0,0 +1,8 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Astro Blog";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+
+export const MASTODON_URL = "https://mastodon.nu/@jesperlundberg";
+export const GITHUB_URL = "https://github.com/jesperlundberg";
